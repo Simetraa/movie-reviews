556085b9b98e06646f82acefbb7713bac0d7e241
Simetraa <simetra@simetra.dev>
2025-07-18 21:47:25 +0200

    Implement a basic mobile first design to the movie page

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -27,6 +27,7 @@ export default function MoviePage() {
     )
 
     const [rating, setRating] = useState<number>(0);
+    const [watchlist, setWatchlist] = useState<boolean>(false);
 
 
 
@@ -55,7 +56,7 @@ export default function MoviePage() {
             accountStatesData.rated
         ) {
             setRating(accountStatesData.rated.value / 2);
-            console.log("Rated value:", accountStatesData.rated.value);
+            setWatchlist(accountStatesData.watchlist);
         }
     }, [accountStatesData]);
 
@@ -80,6 +81,30 @@ export default function MoviePage() {
         });
     }
 
+    async function handleWatchlistToggle() {
+        console.log("Toggling watchlist for movie ID:", id);
+        const sessionId = Cookies.get("session_id");
+        const url = `https://api.themoviedb.org/3/account/watchlist?session_id=${sessionId}`;
+
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                accept: "application/json",
+                "Content-Type": "application/json;charset=utf-8",
+                Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API}`,
+            },
+            body: JSON.stringify({
+                "media_type": "movie",
+                "media_id": id,
+                "watchlist": !watchlist
+            }),
+        });
+
+        if (!res.ok) {
+            console.error("Failed to add to watchlist");
+        }
+    }
+
 
 
 
@@ -92,12 +117,12 @@ export default function MoviePage() {
 
 
     return <>
-        <div className="px-8 flex flex-col gap-8">
-            <div className="flex gap-8">
-                <Image alt="movie poster" width="300" height="450" src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${detailsData!.poster_path}`}></Image>
+        <div className="px-4 md:px-8 flex flex-col gap-8 w-full">
+            <div className="flex-col md:flex-row flex gap-8">
+                <Image className="w-full md:w-[300px]" alt="movie poster" width="300" height="450" src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2${detailsData!.poster_path}`}></Image>
                 <div className="flex flex-col gap-4">
                     <h1 className="text-2xl">{detailsData!.title} <span className="text-neutral-600">({dateFromString(detailsData!.release_date).getFullYear()})</span></h1>
-                    <div className="flex gap-4">
+                    <div className="flex gap-4 flex-wrap">
                         <div className="flex gap-2">
                             <Star></Star>
                             <span>{detailsData!.vote_average}</span>
@@ -112,8 +137,8 @@ export default function MoviePage() {
                     </div>
                     <p>{detailsData?.overview}</p>
                     <div className="flex gap-8 flex-row">
-                        <button className="bg-red-700 flex self-start p-2 gap-1 items-center justify-center text-white rounded-sm">
-                            <Plus /> Add to watchlist
+                        <button className="bg-red-700 flex self-start p-2 gap-1 items-center justify-center text-white rounded-sm" onClick={() => handleWatchlistToggle}>
+                            <Plus />{watchlist ? "Remove from watchlist" : "Add to watchlist"}
                         </button>
                         <div className="flex items-center gap-4">
                             <Rating onChange={handleRatingChange} value={rating}>
@@ -134,17 +159,20 @@ export default function MoviePage() {
                 <h2 className="text-xl">Reviews</h2>
                 <div className="flex flex-col gap-2">
                     {reviewsDetails?.results.map((review) => (
-                        (
-                            <div key={review.id} className="p-4 border rounded-md">
+                        <div key={review.id} className="p-4 border rounded-md">
+                            <div className="flex items-center gap-2">
                                 <h3 className="text-lg font-semibold">{review.author}</h3>
+                                <Separator orientation="vertical" />
+                                <span className="text text-neutral-700">{dateFromString(review.created_at).toLocaleDateString()}</span>
+                            </div>
+                            <div>
                                 <div className="text-sm text-neutral-700">
                                     <ShowMoreText text={review.content} />
                                 </div>
                             </div>
-                        )
+                        </div>
                     ))}
                 </div>
-
             </div>
         </div>
     </>
