801a7167718d34d15703d98717a92ad28937032b
Simetraa <simetra@simetra.dev>
2025-07-26 20:41:35 +0200

    Add contents to account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -8,9 +8,16 @@ import { AccountHeader } from "../components/account-header";
 import { WatchlistRow } from "../components/watchlist-row";
 import Gravatar from "react-gravatar";
 import { Separator } from "@/components/ui/separator";
-import { BinocularsIcon, Flag, Heart, List, Star } from "lucide-react";
+import { BinocularsIcon, Flag, Heart, List, ListRestart, Star } from "lucide-react";
 import emojiFlags from 'emoji-flags';
 import ReactCountryFlag from "react-country-flag";
+import { Spinner } from "@/components/ui/spinner";
+import Link from "next/link";
+import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import pluralize from "pluralize";
+import { MovieCardHorizontal } from "../components/movie-card-horizontal";
+import { Movie } from "@/types/Movie";
+import { Badge } from "@/components/ui/badge";
 
 const fetcher = async (url: string) => {
     const token = `Bearer ${process.env.NEXT_PUBLIC_TMDB_API}`
@@ -41,6 +48,27 @@ export default function AccountPage() {
         { refreshInterval: 0, shouldRetryOnError: false }
     )
 
+    const { data: listData, error: listsError, isLoading: isListsLoading } = useSWR<PaginatedResponse<List>>(
+        `https://api.themoviedb.org/3/account/${memoizedId}/lists?session_id=${sessionId}`,
+        fetcher
+    )
+
+    const { data: watchlistData, error: watchlistError, isLoading: isWatchlistLoading } = useSWR<PaginatedResponse<Movie>>(
+        `https://api.themoviedb.org/3/account/${memoizedId}/watchlist/movies?session_id=${sessionId}`,
+        fetcher
+    )
+
+    const { data: ratedMoviesData, error: ratedMoviesError, isLoading: isRatedMoviesLoading } = useSWR<PaginatedResponse<Movie>>(
+        `https://api.themoviedb.org/3/account/${memoizedId}/rated/movies?session_id=${sessionId}`,
+        fetcher
+    )
+
+    const { data: favouriteMoviesData, error: favouriteMoviesError, isLoading: isFavouriteMoviesLoading } = useSWR<PaginatedResponse<Movie>>(
+        `https://api.themoviedb.org/3/account/${memoizedId}/favorite/movies?session_id=${sessionId}`,
+        fetcher
+    )
+
+
 
     console.log("Memoised id", memoizedId)
 
@@ -51,7 +79,6 @@ export default function AccountPage() {
 
     return (
         <>
-
             <div className="flex items-center bg-accent gap-4 mx-[-32px] px-[32px] py-4">
                 <Gravatar md5={accountData?.avatar.gravatar.hash}></Gravatar>
                 <h1 className="text-xl">{accountData?.username}</h1>
@@ -65,10 +92,52 @@ export default function AccountPage() {
                 </div>
             </div>
             <h2 className="text-md font-medium"><List className="inline-block mr-2" />Your Lists</h2>
+            {isListsLoading && <Spinner></Spinner>}
             {/* horizontal scrolling list, similar to index */}
+            {listsError && <p>Failed to load lists.</p>}
+            <div className="flex overflow-x-auto gap-4">
+                {listData?.results.map((list) => (
+                    <Link key={list.id} href={`/lists/${list.id}`}>
+                        <Card className="w-48">
+                            <CardHeader>{list.name}</CardHeader>
+                            <CardContent className="flex flex-col gap-2">
+                                <CardDescription>{list.description}</CardDescription>
+                                <div className="flex gap-2">
+                                    <Badge>{pluralize("item", list.item_count, true)}</Badge>
+                                    <Badge>{pluralize("favourites", list.favorite_count, true)}</Badge>
+                                </div>
+                            </CardContent>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+
+            {/* Watchlist Section */}
             <h2 className="text-md font-medium"><BinocularsIcon className="inline-block mr-2" />Watchlist</h2>
+            {isWatchlistLoading && <Spinner></Spinner>}
+            {watchlistError && <p>Failed to load watchlist.</p>}
+            <div className="flex flex-row gap-4 overflow-x-scroll overflow-y-clip">
+                {watchlistData?.results.map((item) => (
+                    <MovieCardHorizontal key={item.id} movie={item} />
+                ))}
+            </div>
             <h2 className="text-md font-medium"><Star className="inline-block mr-2" />Rated Movies</h2>
+            {isRatedMoviesLoading && <Spinner></Spinner>}
+            {ratedMoviesError && <p>Failed to load rated movies.</p>}
+            <div className="flex flex-row gap-4 overflow-x-scroll overflow-y-clip">
+                {ratedMoviesData?.results.map((item) => (
+                    <MovieCardHorizontal key={item.id} movie={item} />
+                ))}
+            </div>
+
             <h2 className="text-md font-medium"><Heart className="inline-block mr-2" />Favorite Movies</h2>
+            {isFavouriteMoviesLoading && <Spinner></Spinner>}
+            {favouriteMoviesError && <p>Failed to load favourite movies.</p>}
+            <div className="flex flex-row gap-4 overflow-x-scroll overflow-y-clip">
+                {favouriteMoviesData?.results.map((item) => (
+                    <MovieCardHorizontal key={item.id} movie={item} />
+                ))}
+            </div>
         </>
     )
 }
