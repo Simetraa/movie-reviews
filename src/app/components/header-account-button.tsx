3e0b3e3f12445184d8b2c7cd417067494d08c02c
Angelina <angelina.geis@study.thws.de>
2025-07-29 18:58:14 +0200

    add logout button

--- src/app/components/header-account-button.tsx
+++ src/app/components/header-account-button.tsx
@@ -5,6 +5,8 @@ import Gravatar from "react-gravatar";
 import { NavigationMenuLink } from "@/components/ui/navigation-menu";
 import { Skeleton } from "@/components/ui/skeleton";
 import Link from "next/link";
+import { Button } from "@/components/ui/button"
+
 
 export function HeaderAccountButton() {
     const sessionId = Cookies.get("session_id");
@@ -15,7 +17,12 @@ export function HeaderAccountButton() {
         { refreshInterval: 0, shouldRetryOnError: false }
     )
 
-    if (accountError || sessionId === undefined) return <NavigationMenuLink asChild>
+    function handleLogout() {
+        Cookies.remove("session_id")
+        window.location.href = "/"
+    }
+
+    if (accountError || !sessionId) return <NavigationMenuLink asChild>
         <Link href="/login">Login</Link>
     </NavigationMenuLink>
     if (isAccountLoading) return <div className="flex flex-row items-center gap-2 p-[8px]">
@@ -24,14 +31,24 @@ export function HeaderAccountButton() {
     </div>
 
     return (
-        <NavigationMenuLink asChild className="flex flex-row items-center gap-2">
-            <Link href="/account">
-                <div className="flex flex-row items-center gap-2">
-                    <Gravatar md5={accountData.avatar.gravatar.hash} className="rounded-lg w-10 h-10" />
-                    <span className="hidden md:block">{accountData.username}</span>
-                </div>
-            </Link>
-        </NavigationMenuLink>
+        <div className="flex items-center gap-2 pr-2">
+            <NavigationMenuLink asChild className="flex flex-row items-center gap-2">
+                <Link href="/account">
+                    <div className="flex flex-row items-center gap-2">
+                        <Gravatar md5={accountData.avatar.gravatar.hash} className="rounded-lg w-10 h-10"/>
+                        <span className="hidden md:block">{accountData.username}</span>
+                    </div>
+                </Link>
+            </NavigationMenuLink>
+            <Button
+                variant="ghost"
+                size="sm"
+                className="text-sm font-normal"
+                onClick={handleLogout}
+            >
+                Logout
+            </Button>
+        </div>
     )
 
 }
