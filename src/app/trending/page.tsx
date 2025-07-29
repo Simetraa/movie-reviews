e8b4ccb7f8dd4b858185708564112e878065f3e8
Simetraa <simetra@simetra.dev>
2025-07-29 10:22:54 +0200

    Fix url for trending page

--- src/app/trending/page.tsx
+++ src/app/trending/page.tsx
@@ -4,7 +4,7 @@ import MovieInfinitePage from "../components/movie-infinite-page";
 export default function TopPage() {
     return (
         <MovieInfinitePage
-            url="https://api.themoviedb.org/3/movie/trending"
+            url="https://api.themoviedb.org/3/trending/movie/day"
             title="Trending Movies"
         />
     );
